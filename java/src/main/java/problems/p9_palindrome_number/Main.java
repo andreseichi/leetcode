6d1312fded5aeb982cbe68d17570f536a36e8184
tashiro <andre_seichi@hotmail.com>
2026-06-10 13:44:04 -0300

    solution problem 9

---
+++ java/src/main/java/problems/p9_palindrome_number/Main.java
@@ -0,0 +1,9 @@
+package problems.p9_palindrome_number;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean result = Solution.isPalindrome(-121);
+
+        System.out.println(result);
+    }
+}
---
+++ java/src/main/java/problems/p9_palindrome_number/Solution.java
@@ -0,0 +1,24 @@
+package problems.p9_palindrome_number;
+
+import java.util.ArrayList;
+
+class Solution {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int control = x;
+        ArrayList<Integer> array = new ArrayList<>();
+
+        while (Math.floor(control) != 0) {
+            Integer digit = (int) Math.floor(control % 10);
+            array.add(digit);
+            control = control / 10;
+        }
+
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) != array.get(array.size() - 1 - i)) return false;
+        }
+
+        return true;
+    }
+}
